266e78e85934b274c2af7bf8e49eebf9f22621b9
perbergstrom12 <per.bergstrom@entryevent.se>
2019-09-24 15:55:24 +0200

    first

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,104 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import './all.sass'
-import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from "gatsby"
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
+import Nav from "../components/Nav";
+import clsx from "clsx";
+import useSiteMetadata from "./SiteMetadata";
+import { withPrefix } from "gatsby";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../theme";
+import { makeStyles } from "@material-ui/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Drawer from "@material-ui/core/Drawer";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import logo from "../Images/logo_dark.svg";
 
-const TemplateWrapper = ({ children }) => {
-  const { title, description } = useSiteMetadata()
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    backgroundColor: theme.app.background[1],
+    boxShadow: "none",
+    borderBottom: `1px solid ${theme.app.borderColor}`,
+    color: theme.app.labelColor
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    boxShadow: "none",
+    borderBottom: `1px solid ${theme.app.borderColor}`,
+    color: theme.app.labelColor
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    color: theme.app.labelColor
+  },
+  drawerButton: {
+    color: theme.app.labelColor
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+  logo: { height: "40px", width: "auto" }
+}));
+
+const TemplateWrapperInner = props => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(true);
+  const { title, description } = useSiteMetadata();
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   return (
     <div>
       <Helmet>
@@ -43,13 +134,70 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content={`${withPrefix("/")}img/og-image.jpg`} />
+        <meta
+          property="og:image"
+          content={`${withPrefix("/")}img/og-image.jpg`}
+        />
       </Helmet>
-      <Navbar />
-      <div>{children}</div>
-      <Footer />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            <img src={logo} alt="logo" className={classes.logo} />
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton
+            onClick={handleDrawerClose}
+            className={classes.drawerButton}
+            aria-label="close drawer"
+          >
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <Nav />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <div>{props.children}</div>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default TemplateWrapper
+const TemplateWrapper = props => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <TemplateWrapperInner {...props} />
+  </ThemeProvider>
+);
+
+export default TemplateWrapper;
